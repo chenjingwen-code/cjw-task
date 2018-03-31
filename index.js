ab4065326883ddb2b46c63b93d85d095ac20b387
chenjingwen-code <756160054@163.com>
2018-03-31 13:45:21 +0800

    all

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('public'))
+app.use(express.static('public/CSS'))
+app.use(express.static('public/JS'))
 
 app.get('/', function (req, res) {
   res.send('Hello World')
