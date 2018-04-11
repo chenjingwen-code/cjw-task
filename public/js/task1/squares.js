659ff2239e4feb7a4b05720c9dd12f0e5e4de2eb
chenjingwen-code <756160054@163.com>
2018-04-11 19:26:39 +0800

    push code

--- public/js/task1/squares.js
+++ public/js/task1/squares.js
@@ -1,7 +1,6 @@
 /**
  * Created by Administrator on 2018/4/10.
  */
-var text;
 function numrandom(){
             var blocks=Array(0,1,2,3,4,5,6,7,8);
             var newblock=new Array();
@@ -37,6 +36,7 @@ function circle(){
 
 var text;
 function starting(){
+    clearInterval(text);
     text=setInterval("circle()",1000);
 }
 
